4f2d2c6a68c7d17e33ec18fccb4367cffe9bbde3
ahmed <ahmedehab.sg@gmail.com>
2026-05-27 05:25:55 +0300

    add problems

--- crates/problems/src/leet_code/arrays_and_hasing/_0049_group_anagrams.rs
+++ crates/problems/src/leet_code/arrays_and_hasing/_0049_group_anagrams.rs
@@ -1,12 +1,26 @@
+use std::collections::HashMap;
+
 #[allow(unused_imports)]
 use utils::Runable;
 
 use crate::Solution;
 
-// problem: https://leetcode.com/problems/two-sum/
+// problem: https://leetcode.com/problems/group-anagrams/description
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        vec![]
+        let mut result: HashMap<[u8; 26], Vec<String>> = HashMap::new();
+
+        for s in strs {
+            let mut key = [0u8; 26];
+
+            for b in s.bytes() {
+                key[(b - b'a') as usize] += 1;
+            }
+
+            result.entry(key).or_insert(Vec::new()).push(s);
+        }
+
+        result.into_values().collect()
     }
 }
 
@@ -22,14 +36,20 @@ impl Runable for Solution {
                 "bat".to_string()
             ],),
             vec![
-                ["bat".to_string()],
-                ["nat".to_string(), "tan".to_string()],
-                ["ate".to_string(), "eat".to_string(), "tea".to_string()]
+                vec!["tan".to_string(), "nat".to_string()],
+                vec!["eat".to_string(), "tea".to_string(), "ate".to_string()],
+                vec!["bat".to_string()]
             ]
         );
 
-        assert_eq!(Self::group_anagrams(vec![""], 6), vec![1, 2]);
+        assert_eq!(
+            Self::group_anagrams(vec!["".to_string()]),
+            vec![vec!["".to_string()]]
+        );
 
-        assert_eq!(Self::group_anagrams(vec!["a"], 6), vec![0, 1]);
+        assert_eq!(
+            Self::group_anagrams(vec!["a".to_string()]),
+            vec![vec!["a".to_string()]]
+        );
     }
 }
