4fc3901417d9e9aaebe08611fff52cb2074c33d4
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2026-05-28 01:53:14 +0300

    add problems

--- crates/problems/src/leet_code/arrays_and_hasing/_0049_group_anagrams.rs
+++ crates/problems/src/leet_code/arrays_and_hasing/_0049_group_anagrams.rs
@@ -24,32 +24,39 @@ impl Solution {
     }
 }
 
-impl Runable for Solution {
-    fn run() {
-        assert_eq!(
-            Self::group_anagrams(vec![
-                "eat".to_string(),
-                "tea".to_string(),
-                "tan".to_string(),
-                "ate".to_string(),
-                "nat".to_string(),
-                "bat".to_string()
-            ],),
-            vec![
-                vec!["tan".to_string(), "nat".to_string()],
-                vec!["eat".to_string(), "tea".to_string(), "ate".to_string()],
-                vec!["bat".to_string()]
-            ]
-        );
-
-        assert_eq!(
-            Self::group_anagrams(vec!["".to_string()]),
-            vec![vec!["".to_string()]]
-        );
-
-        assert_eq!(
-            Self::group_anagrams(vec!["a".to_string()]),
-            vec![vec!["a".to_string()]]
-        );
-    }
-}
+// impl Runable for Solution {
+//     fn run() {
+//         let mut result_1 = Self::group_anagrams(vec![
+//             "eat".to_string(),
+//             "tea".to_string(),
+//             "tan".to_string(),
+//             "ate".to_string(),
+//             "nat".to_string(),
+//             "bat".to_string(),
+//         ]);
+//
+//         let mut expected_1 = vec![
+//             vec!["tan".to_string(), "nat".to_string()],
+//             vec!["eat".to_string(), "tea".to_string(), "ate".to_string()],
+//             vec!["bat".to_string()],
+//         ];
+//
+//         result_1.iter_mut().for_each(|vec_s| vec_s.sort());
+//         result_1.sort();
+//
+//         expected_1.iter_mut().for_each(|vec_s| vec_s.sort());
+//         expected_1.sort();
+//
+//         assert_eq!(result_1, expected_1);
+//
+//         assert_eq!(
+//             Self::group_anagrams(vec!["".to_string()]),
+//             vec![vec!["".to_string()]]
+//         );
+//
+//         assert_eq!(
+//             Self::group_anagrams(vec!["a".to_string()]),
+//             vec![vec!["a".to_string()]]
+//         );
+//     }
+// }
--- crates/problems/src/leet_code/arrays_and_hasing/mod.rs
+++ crates/problems/src/leet_code/arrays_and_hasing/mod.rs
@@ -2,3 +2,5 @@ mod _0001_two_sum;
 mod _0049_group_anagrams;
 mod _0217_contains_duplicate;
 mod _0242_vaild_anagram;
+mod _0271_encode_and_decode_strings;
+mod _0347_top_k_frequent_elements;
